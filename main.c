510338f78214a58d1e97457c213b978e87e342dd
guedeees2004-2 <gabrielguedes2004@aluno.puc-rio.br>
2023-11-19 14:29:18 -0300

    Exercicio completo resolvido

--- main.c
+++ main.c
@@ -18,30 +18,61 @@ static int compInt(int num1, int num2)
     return 0;
 }
 
+int buscaPessoaMaisJovem(int n, pessoa* Clientes, char*nome)
+{
+    int ini = 0;
+    int fim = n - 1;
+    int meio, comp;
+    
+    while (ini <= fim)
+    {
+        meio = (ini + fim)/2;
+        if (strcmp(nome, Clientes[meio].nome) < 0)
+            fim = meio - 1;
+        else if (strcmp(nome, Clientes[meio].nome) > 0)
+            ini = meio + 1;
+        else
+        {
+            while (meio > 0 && strcmp(nome, Clientes[meio - 1].nome) == 0)
+                meio--;
+            return meio;
+        }
+    }
+    return -1;
+}
+
 int buscaPessoa(int n, pessoa* Clientes, char* nome, int idade)
 {
     int ini = 0;
-    int fim = n-1;
+    int fim = n - 1;
     int meio, comp;
 
     while (ini <= fim)
     {
         meio = (ini + fim)/2;
-        if (strcmp(nome, Clientes->nome) < 0)
+        if (strcmp(nome, Clientes[meio].nome) < 0)
             fim = meio - 1;
-        else if (strcmp(nome, Clientes->nome) > 0)
+        else if (strcmp(nome, Clientes[meio].nome) > 0)
             ini = meio + 1;
         else
-            return meio;
+        {
+            if((comp = compInt(idade, Clientes[meio].idade)) < 0)
+                fim = meio - 1;
+            else if (comp > 0)
+                ini = meio + 1;
+            else
+                return meio;
+        }
     }
     return -1;
 }
 
 int main(void)
 {
-    pessoa Clientes[] = {{"ada", 20}, {"ana", 19}};
+    pessoa Clientes[] = {{"ada", 20}, {"ana", 19}, {"ana", 23}, {"ana", 27}, {"ana", 31}, {"bia", 18}, {"eva", 21}, {"lia", 17}, {"rai", 30}, {"zac", 23}, {"zac", 18}};
 
-    printf("%d\n", buscaPessoa(2, Clientes, "ana", 19));
+    printf("Ana de 31 anos esta na posicao: %d\n", buscaPessoa(9, Clientes, "ana", 31));
+    printf("A ana mais jovem esta na posicao: %d\n", buscaPessoaMaisJovem(9, Clientes, "ana"));
 
     return 0;
 }
